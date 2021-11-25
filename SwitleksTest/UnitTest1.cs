a1d1729d195dc03708b61bc721b61951d4f551f7
SwMikolaj <mikolaj99x@gmail.com>
2021-11-25 22:07:38 +0100

    Main test point A

--- SwitleksTest/UnitTest1.cs
+++ SwitleksTest/UnitTest1.cs
@@ -13,7 +13,22 @@ namespace SwitleksTest
         public String loto()
         {
             string abc = "abc";
+            Random rand = new Random();
+
             return abc;
+        }
+       public void addProduct(int x , IWebDriver driver)
+        {
+            IWebElement button = driver.FindElement(By.ClassName("add"));
+           
+            IWebElement more = driver1.FindElement(By.ClassName("bootstrap-touchspin-up"));
+            for (int i = 1; i < x; i++)
+            {
+                more.Click();
+            }
+             button.Click();
+            Thread.Sleep(1000);
+            driver1.Navigate().Back();
         }
         IWebDriver driver1;
           [SetUp]
@@ -26,28 +41,81 @@ namespace SwitleksTest
             driver1.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(5);
             driver1.Manage().Timeouts().PageLoad = System.TimeSpan.FromSeconds(5);
         }
-       // [Test]
-        public void Test1()
+        [Test]
+        public void MainTest()
         {
             Uri sklepUrl = new Uri("https://presta.bielinski.dev/pl/");
             driver1.Navigate().GoToUrl(sklepUrl);
             string expectedUrl = "https://presta.bielinski.dev/pl/";    // oczekiwany adres strony
             Assert.AreEqual(expectedUrl, driver1.Url, "No to nie nasz sklep mordo");
 
-            // czego szukamy
-            IWebElement lampy1 = driver1.FindElement(By.LinkText("LAMPY"));
+            // kategoria pierwsza 
+            {
+                IWebElement lampy1 = driver1.FindElement(By.LinkText("LAMPY"));
+                lampy1.Click();
 
-            lampy1.Click();
+                IWebElement product = driver1.FindElement(By.CssSelector("[data-id-product='102']"));
+                product.Click();
 
-            Uri sklepLampy = new Uri("https://presta.bielinski.dev/pl/6-lampy");
-            Assert.AreEqual(sklepLampy, driver1.Url, "TO NIE LAMPY");
+                addProduct(1, driver1);
 
 
+                product = driver1.FindElement(By.CssSelector("[data-id-product='103']"));
+                product.Click();
+
+                addProduct(2, driver1);
+
+                product = driver1.FindElement(By.CssSelector("[data-id-product='101']"));
+                product.Click();
+
+                addProduct(3, driver1);
+
+                product = driver1.FindElement(By.CssSelector("[data-id-product='111']"));
+                product.Click();
+
+                addProduct(5, driver1);
+
+                product = driver1.FindElement(By.CssSelector("[data-id-product='107']"));
+                product.Click();
+
+                addProduct(20, driver1);
+            }
+            // kategoria druga
+            {
+                IWebElement zarowki = driver1.FindElement(By.LinkText("¯ARÓWKI LED"));
+                zarowki.Click();
+
+                IWebElement product = driver1.FindElement(By.CssSelector("[data-id-product='29']"));
+                product.Click();
+
+                addProduct(10, driver1);
+
+                product = driver1.FindElement(By.CssSelector("[data-id-product='31']"));
+                product.Click();
+
+                addProduct(2, driver1);
+
+                product = driver1.FindElement(By.CssSelector("[data-id-product='28']"));
+                product.Click();
+
+                addProduct(2, driver1);
+
+                product = driver1.FindElement(By.CssSelector("[data-id-product='34']"));
+                product.Click();
+
+                addProduct(5, driver1);
+
+                product = driver1.FindElement(By.CssSelector("[data-id-product='37']"));
+                product.Click();
+
+                addProduct(20, driver1);
+            }
+            IWebElement bascet = driver1.FindElement(By.ClassName("shopping-cart"));
+            bascet.Click();
         }
 
-
         
-        [Test]
+       // [Test]
         public void TestBuing()
         {
             Uri sklepUrl = new Uri("https://presta.bielinski.dev/pl/");
@@ -83,7 +151,7 @@ namespace SwitleksTest
 
 
 
-          [Test]
+        //  [Test]
         public void TestMessageToSupport()
         {
             Uri sklepUrl = new Uri("https://presta.bielinski.dev/pl/");
